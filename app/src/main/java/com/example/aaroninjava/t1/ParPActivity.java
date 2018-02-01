b1b07b56aef11857498406444ef1a8a6221ff94e
aarontsai <aaroninpcschool241@gmail.com>
2018-02-02 00:36:23 +0800

    Layout  Parameter  Test

---
+++ app/src/main/java/com/example/aaroninjava/t1/ParPActivity.java
@@ -0,0 +1,13 @@
+package com.example.aaroninjava.t1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_par_p);
+    }
+}
